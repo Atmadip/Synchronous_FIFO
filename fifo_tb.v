214da85553c512c005bb8c1df75735f78d9e3181
atmadip <atmadeep170@gmail.com>
2024-04-24 20:09:10 -0500

    final commit

--- fifo_tb.v
+++ fifo_tb.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/03/2024 12:59:23 PM
-// Design Name: 
-// Module Name: fifo_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module fifo_tb;
